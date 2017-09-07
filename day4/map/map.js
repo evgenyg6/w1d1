31f8488d6f3b98c70c38fe763df9fc8447756929
evgenyg6 <evgenyg@live.com>
2017-09-07 21:06:47 +0000

    Uploaded Callbacks - map function

---
+++ day4/map/map.js
@@ -0,0 +1,23 @@
+function map(arrArg, callback){
+  var arr = [];
+  arrArg.forEach(function(element){
+     arr.push(callback(element));
+
+  })
+     return console.log(arr);
+
+}
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
