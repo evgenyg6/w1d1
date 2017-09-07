93e90ac7965e76fcd340fcafe967cedab934b913
evgenyg6 <evgenyg@live.com>
2017-09-07 23:06:26 +0000

    Added loadedDice and countdownGenerator

---
+++ day4/countDown/countDown.js
@@ -0,0 +1,25 @@
+var countdownGenerator = function (x) {
+
+var time = x;
+  return function(){
+
+ if (time > 0) {
+    console.log("T-minus" + time);
+  }
+  if (time === 0) {
+    console.log("Blast Off!");
+  }
+  if (time < 0) {
+    console.log("Rockets already gone, bub!");
+  }
+  time -= 1;
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
--- day4/map/map.js
+++ day4/map/map.js
@@ -1,16 +1,16 @@
-function map(arrArg, callback){
+function map(arrArg, callback){  //arguments are words, function(word) from below
   var arr = [];
-  arrArg.forEach(function(element){
-     arr.push(callback(element));
+  arrArg.forEach(function(element){  //for each element in aarArg
+     arr.push(callback(element)); //first, callback function, then push into new array
 
   })
-     return console.log(arr);
+     return console.log(arr); //must console log to show results
 
 }
 
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function(word) {
+map(words, function(word) {  //2 parameters: words, function(words)
   return word.length;
 });
 
